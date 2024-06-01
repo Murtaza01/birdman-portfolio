73ab16c8b0f9ce2b97ec480bb2602ecec47555ea
Murtaza01 <merdada2000@hotmail.com>
2024-06-01 20:44:05 +0300

    added a navbar, with hamburger logic

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Nav from "./components/Nav";
+
+const App = () => {
+  return (
+    <>
+      <Nav />
+      <h1 className="text-center">hello world</h1>
+    </>
+  );
+};
+
+export default App;
